d8afe777f86652643382f57cfcfacea6b8990928
Robert Stefan <rpahonie@yahoo.com>
2023-01-31 20:01:17 +0100

    Add project files.

---
+++ Palindrome_Project/Palindrome.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Project
+{
+    class Palindrome
+    {
+      public  static bool IsPalindrome(string text)
+        {
+            string result = "";
+            text = text.ToLower();
+
+            if (text.Contains(" "))
+            {
+                text = text.Replace(" ", "");
+            }
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                result += text[i];
+            }
+
+            return result == text;
+        }
+
+        public static bool IsPalindrome2(string text)
+        {
+            text = text.ToLower();
+            bool flag = false;
+            text = text.Replace(" ", "");
+
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                if (text[i] == text[text.Length -1 -i])
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            return flag;
+        }
+
+        public static bool IsPalindromeNumber(int n)
+        {
+            string convertedNumber = n.ToString();
+
+            return IsPalindrome(convertedNumber);
+        }
+
+        public static bool IsPalindromeNumber2(int n)
+        {
+            int temp = n;
+            int result = 0;
+            int lastDigit;
+
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                result = result * 10 + lastDigit;
+                n /= 10;
+            }
+
+            return temp == result;
+        }
+    }
+}
---
+++ Palindrome_Project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Palindrome.IsPalindrome("Rotator"));  // true
+            Console.WriteLine(Palindrome.IsPalindrome("Rotation")); // false
+            Console.WriteLine(Palindrome.IsPalindrome("never odd or even")); // true
+
+            Console.WriteLine(Palindrome.IsPalindrome2("Rotator")); // true
+            Console.WriteLine(Palindrome.IsPalindrome2("Rotation")); // false
+            Console.WriteLine(Palindrome.IsPalindrome2("never odd or even")); // true
+
+            Console.WriteLine(Palindrome.IsPalindromeNumber(1331)); // true
+            Console.WriteLine(Palindrome.IsPalindromeNumber(1371)); // false
+
+            Console.WriteLine(Palindrome.IsPalindromeNumber2(1331)); // true
+            Console.WriteLine(Palindrome.IsPalindromeNumber2(1371)); // false
+
+            Console.ReadKey();
+        }
+    }
+}
